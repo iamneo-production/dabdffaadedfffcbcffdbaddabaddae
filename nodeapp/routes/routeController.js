9c67300537a334477584ae82e6a413e18c270b69
Iamneo <techadmin@iamneo.ai>
2023-09-29 08:01:03 +0000

    Backup Commit

--- nodeapp/routes/routeController.js
+++ nodeapp/routes/routeController.js
@@ -67,3 +67,37 @@
 // });
 
 // module.exports = router;
+
+
+
+
+const express = require('express');
+const userController = require('../controllers/userController');
+const cartController = require('../controllers/cartController');
+const productController = require('../controllers/productController');
+const orderController = require('../controllers/orderController');
+const router = express.Router();
+
+// Login and Signup Routes
+router.post('/login', userController.userLogin);
+router.post('/signup', userController.createUser);
+
+// Product Routes
+router.get('/products', productController.getAllProducts);
+router.post('/admin/addProduct', productController.addProduct);
+router.put('/admin/editProduct/:id', productController.updateProduct);
+router.delete('/admin/deleteProduct/:id', productController.deleteProduct);
+
+// Cart Routes
+router.get('/user/:id/cartitems', cartController.getAllProductsFromCart);
+router.post('/user/addcart', cartController.insertCart);
+router.delete('/user/deleteCart/:id', cartController.removeFromCart);
+router.delete('/user/deleteallcartitems', cartController.deleteAllItemsFromCart);
+
+// Order Routes
+router.get('/user/:id/orders', orderController.getAllOrders);
+router.post('/user/addorder', orderController.addOrder);
+router.delete('/user/deleteorder/:id', orderController.deleteOneOrder);
+router.delete('/user/deleteall', orderController.deleteAllOrders);
+
+module.exports = router;
