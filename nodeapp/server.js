1be4d3fd54ed43790d3283c8bdfea6d24b28cc32
Iamneo <techadmin@iamneo.ai>
2023-09-29 07:36:03 +0000

    Backup Commit

--- nodeapp/server.js
+++ nodeapp/server.js
@@ -1,8 +1,12 @@
+
+const mongoose = require('mongoose');
+const mongoConfig = require('./DBConfig/Database');
 const express = require('express');
 const app = express();
 const PORT = 8080;
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
+
 
 const cors = require('cors');
 
@@ -12,7 +16,31 @@ app.use(cors({
 
 app.disable("x-powered-by");
 
-//Sequelize
+
+
+
+ // Path to your MongoDB configuration file
+
+// Construct the MongoDB connection URL
+// const mongoURL = `mongodb://${mongoConfig.HOST}:${mongoConfig.PORT}/${mongoConfig.DB}`;
+
+// Optional: If you have authentication enabled, you can include the username and password in the URL
+const mongoURL = `mongodb://${mongoConfig.USER}:${mongoConfig.PASSWORD}@${mongoConfig.HOST}:${mongoConfig.PORT}/${mongoConfig.DB}`;
+
+// Connect to MongoDB
+mongoose
+  .connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+  Sequelize
 const database = require("./models");
 database.sequelize.sync()
   .then(() => {
